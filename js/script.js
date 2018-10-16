f1a48a5d3c201786132aae14e6c110cc9d192cb2
OskarGrzelak <oskargrzelak@gmail.com>
2018-10-16 12:19:02 +0200

    animations

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-/* document.querySelector('#about-me-btn').addEventListener('click', scrollIt(document.querySelector('#about-me')));
+// SCROLLING ANIMATIONS
+
+document.querySelector('#about-me-btn').addEventListener('click', (e) => {
+  e.preventDefault();
+  scrollIt(document.querySelector('#about-me'))
+});
+
+document.querySelector('#contact-me-btn').addEventListener('click', (e) => {
+  e.preventDefault();
+  scrollIt(document.querySelector('#contact-me'))
+});
+
+document.querySelector('#portfolio-btn').addEventListener('click', (e) => {
+  e.preventDefault();
+  scrollIt(document.querySelector('#portfolio'))
+});
 
 function scrollIt(element) {
     window.scrollTo({
@@ -6,8 +21,66 @@ function scrollIt(element) {
       'left': 0,
       'top': element.offsetTop
     });
-  } */
+  }
 
+// ANIMATION ON SCROLL HANDLER
+
+const animateHTML = () => {
+  let elems, windowHeight;
+    function init() {
+      elems = document.querySelectorAll('.hidden');
+      windowHeight = window.innerHeight;
+      addEventHandlers();
+      checkPosition();
+    }
+    function addEventHandlers() {
+      window.addEventListener('scroll', checkPosition);
+      window.addEventListener('resize', init);
+    }
+    function checkPosition() {
+      for (let i = 0; i < elems.length; i++) {
+        let positionFromTop = elems[i].getBoundingClientRect().top;
+        if (positionFromTop - windowHeight <= 0) {
+          if (window.innerWidth > 600) {
+            elems[i].classList.remove('hidden');
+            elems[i].classList.replace(
+              'hidden-right',
+              'move-in-right'
+            );
+            elems[i].classList.replace(
+              'hidden-left',
+              'move-in-left'
+            );
+            elems[i].classList.replace(
+              'hidden-fade',
+              'fade'
+            );
+          } else {
+            elems[i].classList.remove('hidden');
+            elems[i].classList.replace(
+              'hidden-right',
+              'move-in-right-phone'
+            );
+            elems[i].classList.replace(
+              'hidden-left',
+              'move-in-left-phone'
+            );
+            elems[i].classList.replace(
+              'hidden-fade',
+              'fade'
+            );
+          }
+        }
+      }
+    }
+    return {
+      init: init
+    };
+  };
+  animateHTML().init();
+
+
+// PORTFOLIO SLIDER
 
 const next = document.querySelector('#next');
 next.addEventListener('click', function() {
